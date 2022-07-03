46433d48c19f2e3985653a99208868860e11b130
Amauri Oliveira <amauriibate32@hotmail.com>
2022-07-03 20:12:03 -0300

    Fix code, add SVG and add diagram

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,9 @@ import routes from './routes';
 const app = express();
 
 app.use(express.json());
-app.use(express.json());
 app.use(cors());
 app.use(expressLayout);
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
