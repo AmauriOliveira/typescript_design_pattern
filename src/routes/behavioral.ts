6ec82998d42d0181de9ddfbf5c796747f86b7b97
Amauri Oliveira <amauriibate32@hotmail.com>
2022-07-09 23:06:26 -0300

    Add TemplateMethod to GUI

--- src/routes/behavioral.ts
+++ src/routes/behavioral.ts
@@ -1,10 +1,24 @@
 import { Request, Response, Router } from 'express';
-import { After, Before } from '../patterns/behavioral/Strategy';
+import { After as StrategyAfter, Before as StrategyBefore } from '../patterns/behavioral/Strategy';
+import {
+  After as TemplateMethodAfter,
+  Before as TemplateMethodBefore,
+} from '../patterns/behavioral/TemplateMethod';
 
 const behavioralRouter = Router();
 
 behavioralRouter.get('/strategy', (request: Request, response: Response) =>
-  response.render('patterns/behavioral/strategy', { after: After(), before: Before() }),
+  response.render('patterns/behavioral/strategy', {
+    after: StrategyAfter(),
+    before: StrategyBefore(),
+  }),
+);
+
+behavioralRouter.get('/template_method', (request: Request, response: Response) =>
+  response.render('patterns/behavioral/template_method', {
+    after: TemplateMethodAfter(),
+    before: TemplateMethodBefore(),
+  }),
 );
 
 export default behavioralRouter;
